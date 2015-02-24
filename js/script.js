6ba88239a6198536f28a5997e933282320a803d3
Ivan <git@ivanhayes.com>
2015-02-24 08:35:20 +0000

    added comments and deleted sass cache.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var menuIcon = document.getElementById('menuicon');
 var wrapper = document.getElementById('wrapper');
+
+// the 'bannerImage' variable is never used.
 var bannerImage = document.getElementById('bannerimage');
+
 var banner = document.getElementById('banner');
 var navBar = document.getElementById('navbar');
 var navHeader = document.getElementById('navheader');
@@ -24,6 +27,9 @@ function closeMenu(){
 	logo.className = 'slideright';
 }
 
+// I don't think you need this resize listener/handler at all.
+// Everything you are doing here could be done with CSS and
+// media queries alone.
 window.onresize = removeMenuClass;
 
 function removeMenuClass(){
@@ -47,6 +53,19 @@ menuIcon.onclick = showHideMenu;
 
 function showHideMenu(){
 
+	// You don't need to use classNames for checking something like this.
+	// instead, create a variable such is 'isNavOpen', and flick it between
+	// true and false, e.g.
+	// if (isNavMenuOpen) {
+
+	// 	/* change classNames */
+	// 	isNavMenuOpen = false;
+	// }
+	// else {
+
+	// 	/* change classNames */
+	// 	isNavMenuOpen = true;
+	// }
 	if(navBar.className !== 'menushow menu'){
 		navBar.className = 'menushow menu';
 		wrapper.className = 'slideleft';
@@ -79,6 +98,9 @@ for(var i = 0; i < bannerImageSource.length; i++){
 //bannerimage.addEventListener("webkitAnimationStart", nextbanner);
 //bannerimage.addEventListener("animationstart", nextbanner);
 
+// don't mix single and double quotes in your project.
+// also, why is this called 'nextBanner'? That name doesn't really coincide with
+// what this function does. something like 'createBanner', or 'init' would be better.
 document.addEventListener("DOMContentLoaded", nextBanner);
 
 banner.style.width = 100 * bannerImageSource.length + '%';
@@ -90,11 +112,19 @@ function nextBanner(){
 		var image = document.createElement('img');
 		image.setAttribute('src', bannerImageSource[i]);
 		image.style.width = imageWidth;
+
+		// set the float using the CSS class. There's no need to do it with JS.
 		image.style.float = 'left';
+
+
 		image.className = 'bannerslide';
+
+		// use triple equals, instead of double, for this kind of comparison.
+		// also, why are you reseting the variable 'i'? It seems pointless here...
 		if(i == bannerImageSource.length){
 			i = 0;
 		}
+
 		banner.appendChild(image);
 	}
 
